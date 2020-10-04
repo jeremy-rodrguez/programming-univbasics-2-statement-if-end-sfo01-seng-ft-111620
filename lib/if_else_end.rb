8dddb0558e35406934fdd562b840fdddfbcbf62b
Jeremy Rodriguez <jeremy.rodrguez@yahoo.com>
2020-10-04 04:36:44 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -8,18 +8,3 @@ if current_time % 2 == 0
 else 
   puts "Odd!"
 end
-
-if Time.now.to_i % 2 == 0
-  puts "Even!"
-else
-  puts "Odd!"
-end
-
-current_time = Time.now
-current_time = current_time.to_i
- 
-if current_time % 2 == 0
-  puts "Even!"
-else
-  puts "Odd!"
-end
